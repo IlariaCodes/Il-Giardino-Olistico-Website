3dc3acd4c662bafc4aa7db87160acfb73b7df19b
Lala <emal.example.com>
2024-12-16 16:46:58 +0100

    Hamburger menu functionality

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 function openPDF() {
             window.open('./public/images/pdf/calendar.pdf', '_blank'); 
         }
+
+const hamburgerMenu = document.getElementById('hamburgerMenu');
+const menu = document.getElementById('side-drawer');
+        
+        // Add click event listener to the hamburger button
+        hamburgerMenu.addEventListener('click', () => {
+            menu.classList.toggle('visible');
+            menu.classList.toggle('hidden');
+        });
