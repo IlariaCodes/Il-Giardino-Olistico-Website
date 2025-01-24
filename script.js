037e2c985302ba5a52b693417001544cc590f5ee
Lala <emal.example.com>
2025-01-24 10:12:14 +0100

    Copyright

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-/* Open Calendar PDF */
+// Open Calendar PDF //
 
 function openPDF() {
-            window.open('./public/images/pdf/calendar.pdf', '_blank'); 
-        }
+  window.open('./public/images/pdf/calendar.pdf', '_blank');
+}
 
+// Hamburger menu //
 const hamburgerMenu = document.getElementById('hamburgerMenu');
 const menu = document.getElementById('side-drawer');
-        
-        // Add click event listener to the hamburger button
-        hamburgerMenu.addEventListener('click', () => {
-            menu.classList.toggle('visible');
-            menu.classList.toggle('hidden');
-        });
+
+// Add click event listener to the hamburger button
+hamburgerMenu.addEventListener('click', () => {
+  menu.classList.toggle('visible');
+  menu.classList.toggle('hidden');
+});
+
+// Imposta l'anno corrente dinamicamente //
+document.getElementById('year').textContent = new Date().getFullYear();
